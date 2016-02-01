a869268e3a9de343cee38e130e0a96ac37664e71
Riad Shalaby <riad.shalaby@hannecke-its.de>
2016-02-01 20:44:11 +0100

    added flag and config handling

--- main.go
+++ main.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"fmt"
-	"github.com/golang/example/stringutil"
+	//"github.com/golang/example/stringutil"
 	//"gopkg.in/mgo.v2"
 	//"gopkg.in/mgo.v2/bson"
+	"flag"
+	"gopkg.in/gcfg.v1"
+	"log"
 )
 
+var config struct{
+	Database struct {
+		Url1 string
+	 }
+}
+
+var cfg = config
+
 func main() {
-	fmt.Println(stringutil.Reverse("Hello World"))
-	fmt.Println("Hello World")
-	fmt.Printf("Hello")
+	ip := flag.String("config","config.cfg","sets the config file")
+
+	flag.Parse()
+	fmt.Printf("Using config file '%v' \n", *ip)
+
+	err := gcfg.ReadFileInto(&cfg, *ip)
+	if err != nil {
+		log.Fatal("Failed to parse config data from file %v with err: %s", *ip, err)
+	}
+	fmt.Println(cfg.Database.Url1)
 }
